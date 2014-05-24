c429d97eacc3a21b4ea597ffb674b7318c4e54d3
Munif Gebara Junior <munifgebara@gmail.com>
2014-05-23 21:47:14 -0300

    Entidades criadas. Filtor de Transações

---
+++ src/main/java/br/com/munif/web/filtros/TransaFiltro.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.munif.web.filtros;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *
+ * @author munifgebarajunior
+ */
+@WebFilter(filterName = "TransaFiltro", urlPatterns = {"/*"})
+public class TransaFiltro implements Filter {
+
+    public static final ThreadLocal<EntityManager> tlem = new ThreadLocal<EntityManager>();
+
+    private EntityManagerFactory emf;
+
+    public TransaFiltro() {
+        emf = Persistence.createEntityManagerFactory("criaListaPU");
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        EntityManager em = emf.createEntityManager();
+        tlem.set(em);
+
+        try {
+
+            long inicio = System.currentTimeMillis();
+            em.getTransaction().begin();
+            chain.doFilter(request, response); //Realmente acontece a requisição
+            em.getTransaction().commit();
+            long tempo = System.currentTimeMillis() - inicio;
+            System.out.println("A requisicao levou " + tempo + "ms");
+
+        } catch (Throwable t) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            t.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
