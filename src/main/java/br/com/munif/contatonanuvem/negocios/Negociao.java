3254b382663e917154a191fbf95eb5dce7624166
Munif Gebara Junior <munifgebara@gmail.com>
2014-05-24 09:55:33 -0300

    Web services para listar contatos e inserir .

--- src/main/java/br/com/munif/contatonanuvem/negocios/Negociao.java
+++ src/main/java/br/com/munif/contatonanuvem/negocios/Negociao.java
@@ -7,6 +7,7 @@
 package br.com.munif.contatonanuvem.negocios;
 
 import br.com.munif.contatonanuvem.Contato;
+import br.com.munif.contatonanuvem.Usuario;
 import br.com.munif.web.filtros.TransaFiltro;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -23,6 +24,24 @@ public class Negociao {
         Query q=em.createQuery("from Contato c where c.usuario.pin=:p order by c.nome");
         q.setParameter("p", pin);
         return q.getResultList();
+    }
+
+    public static Usuario recuperaUsuario(String pin) {
+        EntityManager em=TransaFiltro.tlem.get();
+        Query q=em.createQuery("from Usuario usu where usu.pin=:p");
+        q.setParameter("p", pin);
+        q.setMaxResults(1);
+        List<Usuario> lista=q.getResultList();
+        if (lista.isEmpty()){
+            return null;
+        }
+        return (Usuario) q.getResultList().get(0);
+    }
+
+    public static void insere(Object o) {
+        EntityManager em=TransaFiltro.tlem.get();
+        em.persist(o);
+        
     }
     
     
--- src/main/java/br/com/munif/contatonanuvem/ws/Servicos.java
+++ src/main/java/br/com/munif/contatonanuvem/ws/Servicos.java
@@ -5,8 +5,15 @@
  */
 package br.com.munif.contatonanuvem.ws;
 
+import br.com.munif.contatonanuvem.Contato;
+import br.com.munif.contatonanuvem.ContatoAndroid;
+import br.com.munif.contatonanuvem.Email;
+import br.com.munif.contatonanuvem.Telefone;
+import br.com.munif.contatonanuvem.Usuario;
 import br.com.munif.contatonanuvem.negocios.Negociao;
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -30,6 +37,43 @@ public class Servicos {
         String output = "Jersey say : " + msg;
         return Response.status(200).entity(output).build();
     }
+    
+    @GET
+    @Path("/insere/{param}")
+    public Response insere(@PathParam("param")String contatoJson){
+        Gson gson = new Gson();
+        ContatoAndroid ca = gson.fromJson(contatoJson, ContatoAndroid.class);
+        
+        Usuario u=Negociao.recuperaUsuario(ca.getPin());
+        if (u==null) {
+            return Response.status(400).entity("Pin invalido").build();
+        }
+        converteEInsereContato(u, ca);
+        return Response.status(201).entity("OK").build();
+    }
+
+    public void converteEInsereContato(Usuario u, ContatoAndroid ca) {
+        Contato c=new Contato();
+        c.setUsuario(u);
+        c.setNome(ca.getNome());
+        Negociao.insere(c);
+        if (ca.getTelefones()!=null){
+            for (String s:ca.getTelefones()){
+                Telefone t=new Telefone();
+                t.setContato(c);
+                t.setNumero(s);
+                Negociao.insere(t);
+            }
+        }
+        if (ca.getEmails()!=null){
+            for (String s:ca.getEmails()){
+                Email e=new Email();
+                e.setContato(c);
+                e.setEndereco(s);
+                Negociao.insere(e);
+            }
+        }
+    }
 
     @GET
     @Produces("application/json")
@@ -37,7 +81,34 @@ public class Servicos {
     public String lista() {
         System.out.println("Execuntando o método lista ");
         Gson gson = new Gson();
-        return gson.toJson(Negociao.listaContatos("123"));
+        List<ContatoAndroid> aRetornar=new ArrayList<ContatoAndroid>();
+        //converter de Contato para ContatoAndrois
+        List<Contato> listaContatos = Negociao.listaContatos("123");
+        
+        for (Contato c:listaContatos){
+            ContatoAndroid ca = converteContato(c);
+            aRetornar.add(ca);
+        }
+        return gson.toJson(aRetornar);
+    }
+
+    public ContatoAndroid converteContato(Contato c) {
+        ContatoAndroid ca=new ContatoAndroid();
+        ca.setNome(c.getNome());
+        ca.setPin(c.getUsuario().getPin());
+        if (c.getTelefones()!=null){
+            ca.setTelefones(new String[c.getTelefones().size()]);
+            for (int i=0;i<c.getTelefones().size();i++){
+                ca.getTelefones()[i]=c.getTelefones().get(i).getNumero();
+            }
+        }
+        if (c.getEmails()!=null){
+            ca.setEmails(new String[c.getEmails().size()]);
+            for (int i=0;i<c.getEmails().size();i++){
+                ca.getEmails()[i]=c.getEmails().get(i).getEndereco();
+            }
+        }
+        return ca;
     }
 
 }
