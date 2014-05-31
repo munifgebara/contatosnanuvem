de1f5dbe2a1148195afe75e09393f28fb6bb33b7
Munif Gebara Junior <munifgebara@gmail.com>
2014-05-31 11:21:52 -0300

    Trocando Tabela por árvore

--- src/main/java/br/com/munif/contatonanuvem/controle/ClienteControlador.java
+++ src/main/java/br/com/munif/contatonanuvem/controle/ClienteControlador.java
@@ -6,11 +6,15 @@
 package br.com.munif.contatonanuvem.controle;
 
 import br.com.munif.contatonanuvem.Contato;
+import br.com.munif.contatonanuvem.Email;
+import br.com.munif.contatonanuvem.Telefone;
 import br.com.munif.contatonanuvem.Usuario;
 import br.com.munif.contatonanuvem.negocios.Negociao;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 /**
  *
@@ -21,6 +25,14 @@ import javax.faces.bean.SessionScoped;
 public class ClienteControlador {
 
     private List<Contato> listaContatos;
+    private TreeNode raizContatos;
+
+    public TreeNode getRaizContatos() {
+        if (raizContatos == null) {
+            criaRaizContatos();
+        }
+        return raizContatos;
+    }
 
     public String getUsuario() {
         return Negociao.emailLogado();
@@ -32,6 +44,27 @@ public class ClienteControlador {
             listaContatos = Negociao.listaContatos(u.getPin());
         }
         return listaContatos;
+    }
+
+    private void criaRaizContatos() {
+        raizContatos = new DefaultTreeNode("Contatos", null);
+        for (Contato contato : getContatos()) {
+            TreeNode noNome = new DefaultTreeNode(contato.getNome(), raizContatos);
+            if (contato.getTelefones() != null && !contato.getTelefones().isEmpty()) {
+                TreeNode raizTelefones=new DefaultTreeNode("telefones",noNome);
+                for (Telefone tel : contato.getTelefones()) {
+                    TreeNode noTel = new DefaultTreeNode(tel.getNumero(), raizTelefones);
+                }
+            }
+            if (contato.getEmails()!= null && !contato.getEmails().isEmpty()) {
+                TreeNode raizEmail=new DefaultTreeNode("emails",noNome);
+                for (Email email : contato.getEmails()) {
+                    TreeNode noEmail = new DefaultTreeNode(email.getEndereco(), raizEmail);
+                }
+            }
+
+        }
+
     }
 
 }
